355bd64f219f8a2894eceeddea004d37af1ddb0d
Chimoha31 <mokochii1108@gmail.com>
2022-02-09 20:48:41 -0800

    add to cart

--- shopping.js
+++ shopping.js
@@ -1,49 +1,70 @@
 "use strict";
 
 //------------------------
-// API
+// API (Shopping items)
 // ----------------------
 async function callAPI() {
   const res = await fetch("https://fakestoreapi.com/products");
   const products = await res.json();
   console.log(products);
-  
-  const pContainer = document.querySelector('#products_container');
-  
+
+  const pContainer = document.querySelector("#products_container");
+
   products.map((product) => {
     const div = document.createElement("div");
-    div.style.width = '300px';
-    
+
     const img = document.createElement("img");
     img.setAttribute("src", `${product.image}`);
-    img.style.width = "200px";
-    img.style.height = "250px";
     div.appendChild(img);
-    
+
     const h4 = document.createElement("h4");
     h4.textContent = product.title;
     div.appendChild(h4);
     // h4.style.display = "none";
-    
+
     const p = document.createElement("p");
     p.textContent = `$${product.price}`;
     div.appendChild(p);
+
+    const addButton = document.createElement("button");
+    addButton.textContent = "ADD TO CART";
+    div.appendChild(addButton);
     pContainer.appendChild(div);
+
+    // let cartItemNum = 0;
+    // addButton.addEventListener("click", function () {
+    //   const cart = document.querySelector("span");
+    //   cart.textContent = cartItemNum;
+    //   cartItemNum ++;;
+    //   console.log("CartItemNum", cartItemNum);
+    //   console.log("cart", cart);
+    // });
   });
   console.log(pContainer);
+
+  const buttons = document.querySelectorAll("button");
+  let numArr = [];
+  let num = 0;
+  const cart = document.querySelector("span");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+      cart.textContent = num + 1;
+      num++;
+      console.log("CartItemNum", num);
+      console.log("cart", cart);
+      numArr.push(num);
+      console.log(numArr);
+    });
+  }
+  // console.log(buttons);
 }
 callAPI();
 
 //------------------------
 // Slides
 // ----------------------
-const slide01 = document.querySelector('.img_1');
-const p01 = document.createElement('p');
-p01.textContent = 'Everything at least 10% OFF';
+const slide01 = document.querySelector(".img_1");
+const p01 = document.createElement("p");
+p01.textContent = "Everything at least 10% OFF";
 slide01.appendChild(p01);
 console.log(slide01);
-
-//------------------------
-// Shopping Item
-// ----------------------
-
