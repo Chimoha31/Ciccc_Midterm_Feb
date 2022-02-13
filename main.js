17f3bb16ff1458ef10e31468f7269a2ad8e32df1
Chimoha31 <mokochii1108@gmail.com>
2022-02-12 16:12:15 -0800

    Delete unnecessary codes

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-'use strict';
-const moonIcon = document.querySelector('#icon i:nth-child(1)');
-const hatIcon = document.querySelector('#icon .fa-redhat');
-const lipIcon = document.querySelector('.lip img');
-const iconDiv = document.querySelector('#icon');
-const moonDivA = document.querySelector('.moon a');
-const hatDivA = document.querySelector('.hat a');
-const lipDivA = document.querySelector('.lip a');
+// 'use strict';
+// const moonIcon = document.querySelector('#icon i:nth-child(1)');
+// const hatIcon = document.querySelector('#icon .fa-redhat');
+// const lipIcon = document.querySelector('.lip img');
+// const iconDiv = document.querySelector('#icon');
+// const moonDivA = document.querySelector('.moon a');
+// const hatDivA = document.querySelector('.hat a');
+// const lipDivA = document.querySelector('.lip a');
 
 
-const p1 = document.createElement('p');
-moonIcon.addEventListener('mouseenter', function() {
-  p1.textContent = 'Shopping';
-  moonDivA.appendChild(p1);
-  p2.textContent = '';
-  p3.textContent = '';
-  console.log(moonDivA);
-});
-const p2 = document.createElement('p');
-hatIcon.addEventListener('mouseenter', function() {
-  p2.textContent = 'Company';
-  hatDivA.appendChild(p2);
-  p1.textContent = '';
-  p3.textContent = '';
-  console.log(hatDivA);
-});
-const p3 = document.createElement('p');
-lipIcon.addEventListener('mouseenter', function() {
-  p3.textContent = 'Contact';
-  lipDivA.appendChild(p3);
-  p1.textContent = '';
-  p2.textContent = '';
-  console.log(hatDivA);
-});
+// const p1 = document.createElement('p');
+// moonIcon.addEventListener('mouseenter', function() {
+//   p1.textContent = 'Shopping';
+//   moonDivA.appendChild(p1);
+//   p2.textContent = '';
+//   p3.textContent = '';
+//   console.log(moonDivA);
+// });
+// const p2 = document.createElement('p');
+// hatIcon.addEventListener('mouseenter', function() {
+//   p2.textContent = 'Company';
+//   hatDivA.appendChild(p2);
+//   p1.textContent = '';
+//   p3.textContent = '';
+//   console.log(hatDivA);
+// });
+// const p3 = document.createElement('p');
+// lipIcon.addEventListener('mouseenter', function() {
+//   p3.textContent = 'Contact';
+//   lipDivA.appendChild(p3);
+//   p1.textContent = '';
+//   p2.textContent = '';
+//   console.log(hatDivA);
+// });
